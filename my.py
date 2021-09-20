4e0dae5f4be43cf69c0ea9f91fc56b65c4c44ca8
cmulliss <cmulliss@gmail.com>
2021-09-20 17:44:32 +0100

    refresher, variables and strings

---
+++ my.py
@@ -0,0 +1,12 @@
+name = "me"
+other = "you"
+
+# f string allows us to embed variables in strings.
+greeting = f"Hello {name} and {other}"
+
+print(greeting)
+
+name = "them"
+
+# or can do
+print(f"Hello {name} and {other}")
