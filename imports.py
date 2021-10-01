107d26087de36cd3878b99b9f3544e2f7e3d183c
cmulliss <cmulliss@gmail.com>
2021-10-01 17:11:43 +0100

    errors

--- imports.py
+++ imports.py
@@ -1,3 +1,6 @@
 from mymodule import divide
+import sys
 
 print(divide(10, 2))
+
+print(sys.modules)
---
+++ imports/errors.py
@@ -0,0 +1,23 @@
+def divide(dividend, divisor):
+    if divisor == 0:
+        raise ZeroDivisionError("Divisor cannot be 0.")
+
+    return dividend / divisor
+
+
+grades = []
+
+print("Welcome to the grade average program. ")
+try:
+    average = divide(sum(grades), len(grades))
+
+except ZeroDivisionError as e:
+
+    print("There are no grades yet in your list")
+else:
+    print(f"The average grade is {average}")
+finally:
+    print("Thank you!")
+
+
+# use try except to give custom error messages.
