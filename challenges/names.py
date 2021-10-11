6c83d8c2e834464b99024ffd8cf46ac4e734e7a4
cmulliss <cmulliss@gmail.com>
2021-10-11 13:35:41 +0100

    names challenge

---
+++ challenges/names.py
@@ -0,0 +1,8 @@
+def print_full_name(first, last):
+    print(f"Hello {first} {last}! You just delved into python.")
+
+
+if __name__ == "__main__":
+    first = input("Enter your first name: ")
+    last = input("Enter your last name: ")
+    print_full_name(first, last)
